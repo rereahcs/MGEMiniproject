fd04b13c79e84d562f0ac3443106c74d1b9c867a
Yael Schärer <yschaere@hsr.ch>
2021-10-04 14:24:51 +0200

    init MGE Project and add first Layouts

---
+++ BudgeIt/app/src/main/java/ch/ost/rj/mge/budgeit/MainActivity.java
@@ -0,0 +1,14 @@
+package ch.ost.rj.mge.budgeit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
+}
---
+++ BudgeIt/app/src/main/java/ch/ost/rj/mge/budgeit/StatisticActivity.java
@@ -0,0 +1,14 @@
+package ch.ost.rj.mge.budgeit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatisticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistic);
+    }
+}
